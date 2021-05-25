58552eba862126c09d7ad6eecada608b916567d1
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2021-05-25 10:30:52 -0700

    feat: Add filter code

--- react-thecompleteguide/01-expense-tracker/src/components/Expenses/Expenses.js
+++ react-thecompleteguide/01-expense-tracker/src/components/Expenses/Expenses.js
@@ -10,38 +10,29 @@ const Expenses = (props) => {
 
   const filterChangedHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(item => {
+    return item.date.getFullYear().toString() === filteredYear;
+  })
+
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onFilterChanged={filterChangedHandler}
-      />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-      {/* {props.items.map((item, index) => {
-        return <ExpenseItem key={index} expenses={item} />;
-      })} */}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChanged={filterChangedHandler}
+        />
+        {filteredExpenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
